e7fe783e1f5c4b97452106262168427b92aa729b
NekoBamBam <coriannicolas21@gmail.com>
2026-04-07 20:13:47 -0300

    practica terminada

---
+++ santander/imports.py
@@ -0,0 +1,10 @@
+import operaciones
+import utilidades
+
+
+resultado = operaciones.sumar(5, 3)
+utilidades.imprimir_mensaje(f"El resultado de la suma es: {resultado}")
+
+
+nombre = utilidades.obtener_nombre_usuario()
+utilidades.imprimir_mensaje(f"Hola, {nombre}!")
